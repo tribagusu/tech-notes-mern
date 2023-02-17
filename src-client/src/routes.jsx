8a4a093cc2702f63e35869658a697c88f91944dd
tribagus <tribagus.indonesia@gmail.com>
2023-02-17 11:04:08 +0700

    update router

---
+++ src-client/src/routes.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Public />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+
+  {
+    path: "/dash",
+    element: <DashLayout />,
+    children: [
+      {
+        index: true,
+        element: <Welcome />,
+      },
+      {
+        path: "notes",
+        element: <NotesList />,
+      },
+      {
+        path: "users",
+        element: <UsersList />,
+      },
+    ],
+  },
+
+  // {
+  //   path: "*",
+  //   element: <PageError code={404} />,
+  // },
+]);
